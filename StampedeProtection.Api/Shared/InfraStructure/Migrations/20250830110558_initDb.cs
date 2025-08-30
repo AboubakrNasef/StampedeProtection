feff740f489fde2293d24b037df6d85e30c04eaa
Aboubakr Nasef <ana@esignatur.dk>
2025-08-30 14:10:58 +0300

    add migrations

---
+++ StampedeProtection.Api/Shared/InfraStructure/Migrations/20250830110558_initDb.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StampedeProtection.Api.Shared.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Product 1", 10.0m },
+                    { 2, "Product 2", 20.0m },
+                    { 3, "Product 3", 30.0m },
+                    { 4, "Product 4", 40.0m },
+                    { 5, "Product 5", 50.0m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
